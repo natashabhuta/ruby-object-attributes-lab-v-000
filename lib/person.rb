5e78b5943c9369913fd5bf138e395fa03abbe13e
natashabhuta <natasha.bhuta@gmail.com>
2018-08-03 00:02:29 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,12 +3,12 @@ class Person
     @name = persons_name
   end 
   
-def name 
-  @name 
+  def name 
+    @name 
   end 
 end 
 
-def job=(persons_job)
-  @job = persons_job
-end 
+  def job=(persons_job)
+    @job = persons_job
+  end 
 end 
